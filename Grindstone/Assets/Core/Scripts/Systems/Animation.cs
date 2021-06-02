e06bfb7b8b3523ae947c701d9a087493c9323e6a
Pau Sánchez <pausanchez63@gmail.com>
2021-06-02 18:03:11 +0200

    wip

---
+++ Grindstone/Assets/Core/Scripts/Systems/Animation.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Systems
+{
+    public class Animation : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField] private Animator anim;
+        [SerializeField] private Transform playerTransform;
+
+        private bool endCombo = true;
+        private bool hasPunched = false;
+        #endregion
+
+        void Start()
+        {
+            InputManager.Instance.onMove.AddListener(Move);
+            InputManager.Instance.onMoveLeft.AddListener(MoveLeft);
+            InputManager.Instance.onMoveRight.AddListener(MoveRight);
+            InputManager.Instance.onStopMove.AddListener(StopMove);
+
+            InputManager.Instance.onJump.AddListener(Jump);
+            Movement.Instance.onLand.AddListener(Land);
+            
+            InputManager.Instance.onAttack.AddListener(Attack);
+
+            //Movement.Instance.onLedgeGrabed.AddListener(LedgeGrab);
+        }
+
+        #region Methods
+        private void Move()
+        {
+            AlignPlayerSpriteWithMovementDirection();
+            if (!anim.GetBool("Move"))
+            {
+                anim.SetBool("Move", true);
+            }
+        }
+        private void MoveLeft()
+        {
+            AlignPlayerToLeft();
+            if (!anim.GetBool("Move"))
+            {
+                anim.SetBool("Move", true);
+            }
+        }
+        private void MoveRight()
+        {
+            AlignPlayerToRight();
+            if (!anim.GetBool("Move"))
+            {
+                anim.SetBool("Move", true);
+            }
+        }
+
+        private void StopMove()
+        {
+            anim.SetBool("Move", false);
+        }
+
+        private void Jump()
+        {
+            anim.SetTrigger("Jump");
+            anim.SetBool("Falling", true);
+        }
+        private void Land()
+        {
+            anim.SetBool("Falling", false);
+        }
+
+        private void Attack()
+        {
+            anim.SetTrigger("Punch");
+            if (hasPunched)
+            {
+                endCombo = false;
+            }
+            else
+            {
+                hasPunched = true;
+            }
+        }
+        #endregion
+
+        #region Coroutines
+        #endregion
+
+        #region Utils
+        private void AlignPlayerSpriteWithMovementDirection()
+        {
+            if (InputManager.Instance.movementAmount.x >= 0)
+            {
+                AlignPlayerToRight();   
+            }
+            else
+            {
+                AlignPlayerToLeft();
+            }
+        }
+
+        private void AlignPlayerToLeft()
+        {
+            if (playerTransform.rotation.eulerAngles.y < 90)
+            {
+                playerTransform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+
+        private void AlignPlayerToRight()
+        {
+            if (playerTransform.rotation.eulerAngles.y > 90)
+            {
+                playerTransform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+
+        private void EndCombo()
+        {
+            if (endCombo)
+            {
+                anim.SetTrigger("End Combo");
+                hasPunched = false;
+            }
+            endCombo = true;
+        }
+        #endregion
+    }
+}
--- Grindstone/Assets/Core/Scripts/Systems/Movement.cs
+++ Grindstone/Assets/Core/Scripts/Systems/Movement.cs
@@ -4,169 +4,188 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Movement : MonoBehaviour
+namespace Systems
 {
-    [SerializeField] private Rigidbody2D playerRigidbody;
-    [SerializeField] private float movSpeed;
-    [SerializeField] private float movAcceleration;
-    [SerializeField] private float jumpForce;
-
-    private Vector2 m;
-    private bool jumping = false;
-    private bool canMove = true;
-    private BoxCollider2D playerCollider;
-
-    [SerializeField] private float jumpRayDistance;
-    [SerializeField] private float ledgeRayDistance;
-    [SerializeField] private GameObject rightFoot;
-    [SerializeField] private GameObject leftFoot;
-
-    [NonSerialized] public UnityEvent onLedgeGrabed = new UnityEvent();
-    [NonSerialized] public UnityEvent onLand = new UnityEvent();
-
-    private void Start()
+    public class Movement : MonoBehaviour
     {
-        InputManager.Instance.onMove.AddListener(Move);
-        InputManager.Instance.onMoveLeft.AddListener(MoveLeft);
-        InputManager.Instance.onMoveRight.AddListener(MoveRight);
-        InputManager.Instance.onStopMove.AddListener(StopMove);
-        InputManager.Instance.onJump.AddListener(Jump);
-        onLedgeGrabed.AddListener(GrabLedge);
+        #region Variables
+        [NonSerialized] public static Movement Instance;
 
-        playerCollider = this.GetComponent<BoxCollider2D>();
-    }
+        [SerializeField] private Rigidbody2D playerRigidbody;
+        [SerializeField] private float movSpeed;
+        [SerializeField] private float movAcceleration;
+        [SerializeField] private float jumpForce;
 
-    private void FixedUpdate()
-    {
-        if (canMove)
+        private Vector2 m;
+        private bool jumping = false;
+        private bool canMove = true;
+
+        [SerializeField] private float jumpRayDistance;
+        [SerializeField] private float fallRayDistance;
+        [SerializeField] private float ledgeRayDistance;
+        [SerializeField] private GameObject rightFoot;
+        [SerializeField] private GameObject leftFoot;
+
+        [NonSerialized] public UnityEvent onLedgeGrabed = new UnityEvent();
+        [NonSerialized] public UnityEvent onLand = new UnityEvent();
+        #endregion
+
+        private void Awake()
         {
-            transform.Translate(m, Space.World);
-        }
-    }
-
-    #region Methods
-    private void Move()
-    {
-        m = new Vector2(InputManager.Instance.movementAmount.x, 0) * Time.deltaTime * movSpeed;
-    }
-
-    private void MoveLeft()
-    {
-
-        m = new Vector2(-1, 0) * Time.deltaTime * movSpeed;
-    }
-
-    private void MoveRight()
-    {
-        m = new Vector2(1, 0) * Time.deltaTime * movSpeed;
-    }
-
-    private void StopMove()
-    {
-        m = new Vector2(0, 0);
-    }
-
-    private void Jump()
-    {
-        if (IsPlayerGrounded()) 
-        {
-            jumping = true;
-            playerRigidbody.AddForce(Vector3.up * jumpForce);
-            StartCoroutine(AfterJumpChecker());
-        }
-    }
-
-    private void GrabLedge()
-    {
-        playerRigidbody.isKinematic = true;
-        playerRigidbody.velocity = Vector2.zero;
-        canMove = false;
-    }
-
-    //private void Ledge
-    #endregion
-
-    #region Coroutines
-    IEnumerator AfterJumpChecker()
-    {
-        bool takeOff = false;
-        while (!takeOff)
-        {
-            if (!IsPlayerGrounded())
+            if(Instance == null)
             {
-                takeOff = true;
+                Instance = this;
             }
-            yield return null;
-        }
-        while (jumping)
-        {
-            if (takeOff)
+            else
             {
-                if (IsPlayerGrounded())
-                {
-                    jumping = false;
-                    onLand?.Invoke();
-                }
-                else if (CanPlayerGrabOnLedge())
-                {
-                    jumping = false;
-                    onLedgeGrabed?.Invoke();
-                }
+                Destroy(this);
             }
-            yield return null;
         }
-    }
-    #endregion
 
-    #region Utils
-    private bool IsPlayerGrounded()
-    {
-        LayerMask mask = LayerMask.GetMask("Platforms");
-
-        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, -this.transform.up, jumpRayDistance, mask);
-        Debug.DrawRay(this.transform.position, -this.transform.up * jumpRayDistance, Color.magenta, 0.5f);
-        if (hit.collider != null)
+        private void Start()
         {
-            return true;
+            InputManager.Instance.onMove.AddListener(Move);
+            InputManager.Instance.onMoveLeft.AddListener(MoveLeft);
+            InputManager.Instance.onMoveRight.AddListener(MoveRight);
+            InputManager.Instance.onStopMove.AddListener(StopMove);
+            InputManager.Instance.onJump.AddListener(Jump);
+            onLedgeGrabed.AddListener(GrabLedge);
         }
-        RaycastHit2D rHit = Physics2D.Raycast(rightFoot.transform.position, -this.transform.up, jumpRayDistance, mask);
-        Debug.DrawRay(rightFoot.transform.position, -this.transform.up * jumpRayDistance, Color.magenta, 0.5f);
-        if (rHit.collider != null)
-        {
-            return true;
-        }
-        RaycastHit2D lHit = Physics2D.Raycast(leftFoot.transform.position, -this.transform.up, jumpRayDistance, mask);
-        Debug.DrawRay(leftFoot.transform.position, -this.transform.up * jumpRayDistance, Color.magenta, 0.5f);
-        if (lHit.collider != null)
-        {
-            return true;
-        }
-        return false;
-    }
 
-    private bool CanPlayerGrabOnLedge()
-    {
-        LayerMask mask = LayerMask.GetMask("Walls");
-
-        RaycastHit2D rHit = Physics2D.Raycast(this.transform.position, this.transform.right, ledgeRayDistance, mask);
-        Debug.DrawRay(this.transform.position, this.transform.right * ledgeRayDistance, Color.green, 0.5f);
-        if (rHit.collider != null)
+        private void FixedUpdate()
         {
-            if (rHit.collider.name == "Corner")
+            if (canMove)
+            {
+                transform.Translate(m, Space.World);
+            }
+        }
+
+        #region Methods
+        private void Move()
+        {
+            m = new Vector2(InputManager.Instance.movementAmount.x, 0) * Time.deltaTime * movSpeed;
+        }
+
+        private void MoveLeft()
+        {
+
+            m = new Vector2(-1, 0) * Time.deltaTime * movSpeed;
+        }
+
+        private void MoveRight()
+        {
+            m = new Vector2(1, 0) * Time.deltaTime * movSpeed;
+        }
+
+        private void StopMove()
+        {
+            m = new Vector2(0, 0);
+        }
+
+        private void Jump()
+        {
+            if (IsPlayerGrounded(false)) 
+            {
+                jumping = true;
+                playerRigidbody.AddForce(Vector3.up * jumpForce);
+                StartCoroutine(AfterJumpChecker());
+            }
+        }
+
+        private void GrabLedge()
+        {
+            playerRigidbody.isKinematic = true;
+            playerRigidbody.velocity = Vector2.zero;
+            canMove = false;
+        }
+        #endregion
+
+        #region Coroutines
+        IEnumerator AfterJumpChecker()
+        {
+            bool takeOff = false;
+            while (!takeOff)
+            {
+                if (!IsPlayerGrounded(false))
+                {
+                    takeOff = true;
+                }
+                yield return null;
+            }
+            while (jumping)
+            {
+                if (takeOff)
+                {
+                    if (IsPlayerGrounded(true))
+                    {
+                        jumping = false;
+                        onLand?.Invoke();
+                    }
+                    else if (CanPlayerGrabOnLedge())
+                    {
+                        jumping = false;
+                        onLedgeGrabed?.Invoke();
+                    }
+                }
+                yield return null;
+            }
+        }
+        #endregion
+
+        #region Utils
+        private bool IsPlayerGrounded(bool falling)
+        {
+            float rayDistance;
+            if (falling) { rayDistance = fallRayDistance; }
+            else { rayDistance = jumpRayDistance; }
+
+            LayerMask mask = LayerMask.GetMask("Platforms");
+
+            RaycastHit2D hit = Physics2D.Raycast(this.transform.position, -this.transform.up, rayDistance, mask);
+            Debug.DrawRay(this.transform.position, -this.transform.up * rayDistance, Color.magenta, 0.5f);
+            if (hit.collider != null)
             {
                 return true;
             }
-        }
-        RaycastHit2D lHit = Physics2D.Raycast(this.transform.position, -this.transform.right, ledgeRayDistance, mask);
-        Debug.DrawRay(this.transform.position, -this.transform.right * ledgeRayDistance, Color.green, 0.5f);
-        if (lHit.collider != null)
-        {
-            if (lHit.collider.name == "Corner")
+            RaycastHit2D rHit = Physics2D.Raycast(rightFoot.transform.position, -this.transform.up, rayDistance, mask);
+            Debug.DrawRay(rightFoot.transform.position, -this.transform.up * rayDistance, Color.magenta, 0.5f);
+            if (rHit.collider != null)
             {
                 return true;
             }
+            RaycastHit2D lHit = Physics2D.Raycast(leftFoot.transform.position, -this.transform.up, rayDistance, mask);
+            Debug.DrawRay(leftFoot.transform.position, -this.transform.up * rayDistance, Color.magenta, 0.5f);
+            if (lHit.collider != null)
+            {
+                return true;
+            }
+            return false;
         }
-        return false;
+
+        private bool CanPlayerGrabOnLedge()
+        {
+            LayerMask mask = LayerMask.GetMask("Walls");
+
+            RaycastHit2D rHit = Physics2D.Raycast(this.transform.position, this.transform.right, ledgeRayDistance, mask);
+            Debug.DrawRay(this.transform.position, this.transform.right * ledgeRayDistance, Color.green, 0.5f);
+            if (rHit.collider != null)
+            {
+                if (rHit.collider.name == "Corner")
+                {
+                    return true;
+                }
+            }
+            RaycastHit2D lHit = Physics2D.Raycast(this.transform.position, -this.transform.right, ledgeRayDistance, mask);
+            Debug.DrawRay(this.transform.position, -this.transform.right * ledgeRayDistance, Color.green, 0.5f);
+            if (lHit.collider != null)
+            {
+                if (lHit.collider.name == "Corner")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        #endregion
     }
-    #endregion
 }
